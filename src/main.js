c20388bdbbd1a3f7d4ff2d50e64a87cec1ee6814
Arseniy <arsenyzaretsky182@gmail.com>
2025-02-06 17:44:02 +0200

    add js, new favicon

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Import all the JS files
+
+import './js/header.js';
+import './js/hero.js';
+import './js/about.js';
+import './js/benefits.js';
+import './js/projects.js';
+import './js/faq.js';
+import './js/covers.js';
+import './js/reviews.js';
+import './js/work-together.js';

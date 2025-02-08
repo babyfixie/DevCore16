e8898afa274b7001f9053e75fa0d1862510be1ae
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-02-09 02:04:03 +0300

    create Api function

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,6 +1,6 @@
 // import Swiper JS
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Keyboard, Navigation } from 'swiper/modules';
 // import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -19,61 +19,137 @@ const swiperEl = document.querySelector('swiper-container');
 
 const buttonPrev = document.querySelector('.reviews-button-prev');
 const buttonNext = document.querySelector('.reviews-button-next');
-const userName = document.querySelector('.reviews-user-name');
-const userFoto = document.querySelector('.user-foto');
-console.dir(userFoto);
-const userReview = document.querySelector('.reviews-text');
+const listReviews = document.querySelector('.reviews-list');
 
-const getReviews = () => {
-  return axios.get('https://portfolio-js.b.goit.study/api/reviews');
+const getReviewsApi = async () => {
+  const response = await axios.get(
+    'https://portfolio-js.b.goit.study/api/reviews'
+  );
+  return response.data;
 };
 
-const createReview = el => {
-  userReview.textContent = 'el.review';
-  userFoto.setAttribute('src', 'el.avatar_url');
-  userName.textContent = 'el.author';
-  return;
+const getReviews = async () => {
+  try {
+    const searchedReviews = await getReviewsApi();
+
+    if (searchedReviews.length > 0) {
+      createReviews(searchedReviews);
+    } else {
+      createNotFound();
+    }
+  } catch (error) {
+    createNotFound();
+  }
 };
 
-getReviews()
-  .then(response => {
-    console.dir(response);
-    const fullFillReview = response.data.map(element => createReview(element));
-  })
-  .catch(err => {
-    console.log(err);
-  });
+const createReviews = reviews => {
+  const createListEl = reviews
+    .map(
+      ({
+        author,
+        avatar_url,
+        review,
+      }) => `<li class='reviews-list-item swiper-slide'>
+      <picture>
+        <source srcset ='${avatar_url}'
+        type='image/jpeg'>
+        <img 
+          class='photo-review'
+          src= '${avatar_url}'
+        alt ='${author}'
+        width ='48'
+        height ='48'
+        loading ='lazy'/>
+      </picture>
+      <h3 class= 'autor-review'>${author}</h3>
+       <div class='review-text-wraper'>
+      <p class='review-text'>${review}</p>
+      </div>
+    </li>`
+    )
+    .join('');
 
-// swiper parameters
-const swiperParams = {
-  slidesPerView: 1,
+  listReviews.innerHTML = createListEl;
+};
+
+const createNotFound = (message = 'Not found') => {
+  const p = document.createElement('p');
+  p.textContent = message;
+
+  swiperEl.insertAdjacentElement('afterbegin', p);
+};
+
+getReviews();
+
+const reviewsSwiper = new Swiper('.reviews-swiper-container', {
+  modules: [Navigation, Keyboard],
+  speed: 800,
+  grabCursor: true,
+  allowTouchMove: true,
+  direction: 'horizontal',
+  watchOverflow: true,
+  spaceBetween: 16,
+
+  navigation: {
+    nextEl: '.reviews-button-next',
+    prevEl: '.reviews-button-prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
   breakpoints: {
+    320: {
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+    },
     768: {
+      slidesPerGroup: 2,
       slidesPerView: 2,
-      spaceBetween: 16,
     },
     1440: {
-      slidesPerView: 3,
-      spaceBetween: 16,
+      slidesPerGroup: 4,
+      slidesPerView: 4,
     },
   },
-  on: {
-    init() {
-      // ...
-    },
-  },
-};
-
-// now we need to assign all parameters to Swiper element
-Object.assign(swiperEl, swiperParams);
-
-// and now initialize it
-swiperEl.initialize();
-
-buttonNext.addEventListener('click', () => {
-  swiperEl.swiper.slideNext();
 });
 
-buttonPrev.addEventListener('click', () => {
-  swiperEl.swiper.slidePrev();
-});
+//swiper parameters
+// const swiperParams = {
+//   breakpoints: {
+//     320: {
+//       slidesPerGroup: 1,
+//       slidesPerView: 1,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 16,
+//     },
+//     1440: {
+//       slidesPerView: 3,
+//       spaceBetween: 16,
+//     },
+//   },
+//   on: {
+//     init() {
+//       // ...
+//     },
+//   },
+// };
+
+// // now we need to assign all parameters to Swiper element
+// Object.assign(swiperEl, swiperParams);
+
+// // and now initialize it
+// swiperEl.initialize();
+
+// buttonNext.addEventListener('click', () => {
+//   swiperEl.swiper.slideNext();
+// });
+
+// buttonPrev.addEventListener('click', () => {
+//   swiperEl.swiper.slidePrev();
+// });

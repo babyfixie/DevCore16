f1a4e62d07a9fc36b204a24ce63def183ab7207f
leohutnyk <leohutnyk@gmail.com>
2025-02-10 22:13:36 +0100

    update after clone

--- src/js/about.js
+++ src/js/about.js
@@ -1,9 +1,29 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// Accordion
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Inicializa el acordeón usando la biblioteca Accordion
+  const accordion = new Accordion('.about-me-list', {
+    duration: 400, // Duración de la animación
+    showMultiple: false, // Permite abrir solo un elemento a la vez
+    openOnInit: [0], // Abre el primer bloque al cargar la página
+    collapse: true, // Asegura que los demás bloques estén cerrados al inicio
+  });
+});
+
+//======= Swiper
+//======= import Swiper JS
+
 import Swiper from 'swiper';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
 const aboutMeSwiperBtnNextEl = document.querySelector(
   '.about-me-swiper-btn-next'
 );
+
+console.log(aboutMeSwiperBtnNextEl);
 
 aboutMeSwiperBtnNextEl.addEventListener('mousedown', function () {
   setTimeout(() => {
@@ -18,50 +38,36 @@ const aboutSwiper = new Swiper('.about-me-swiper', {
   allowTouchMove: true,
   direction: 'horizontal',
   watchOverflow: true,
-  loop: true,
-  slidesPerView: 2,
 
   navigation: {
     nextEl: '.about-me-swiper-btn-next',
   },
+
   keyboard: {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
   },
-  mousewheel: true,
+
   breakpoints: {
-    320: { slidesPerView: 2 },
-    768: { slidesPerView: 3 },
-    1440: { slidesPerView: 6 },
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
   },
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-  const aboutMeItems = document.querySelectorAll('.about-me-item');
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
 
-  aboutMeItems.forEach(item => {
-    const btn = item.querySelector('.about-me-btn');
-    const texts = item.querySelectorAll('.about-me-item-text');
-    const icon = btn.querySelector('.about-me-icon');
-
-    texts.forEach(text => (text.style.display = 'none'));
-
-    btn.addEventListener('click', () => {
-      const isVisible = texts[0].style.display === 'block';
-
-      aboutMeItems.forEach(otherItem => {
-        otherItem.querySelectorAll('.about-me-item-text').forEach(text => {
-          text.style.display = 'none';
-        });
-        otherItem.querySelector('.about-me-icon').style.transform =
-          'rotate(0deg)';
-      });
-
-      texts.forEach(text => {
-        text.style.display = isVisible ? 'none' : 'block';
-      });
-      icon.style.transform = isVisible ? 'rotate(0deg)' : 'rotate(180deg)';
-    });
-  });
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: true,
 });
